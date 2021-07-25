e06423e4080e024119961303961fec7a0ee1ded9
adarshEr2020 <adarshprajapati31@gmail.com>
2021-07-25 12:34:42 +0530

    [Adarsh]Added: calculate total fare

--- src/test/java/com/bridgelabz/CabInvoiceGenerator/CabInvoiceGeneratorTest.java
+++ src/test/java/com/bridgelabz/CabInvoiceGenerator/CabInvoiceGeneratorTest.java
@@ -1,5 +1,26 @@
 package com.bridgelabz.CabInvoiceGenerator;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class CabInvoiceGeneratorTest {
-    
+	CabInvoiceGenerator cabInvoiceGenerator = new CabInvoiceGenerator();
+
+	@Test
+	public void givenDistanceAndTimeShouldReturnTotalFare() {
+		double distance = 5.0;
+		int time = 15;
+		double fare = cabInvoiceGenerator.calculateFare(distance, time);
+		Assert.assertEquals(65, fare, 0.0);
+	}
+	
+	@Test
+	public void givenLessDistanceAndTimeShouldReturnMinFare() {
+		double distance = 0.1;
+		int time = 2;
+		double fare = cabInvoiceGenerator.calculateFare(distance, time);
+		Assert.assertEquals(5,fare,0.0);
+	}
+	
+
 }
